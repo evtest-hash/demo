2f55cc3c4cde0129beb697899bf10544f8b74f8e
evtest-hash <84323112+evtest-hash@users.noreply.github.com>
2023-03-30 09:53:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 
 // 1. Try generating with command K on a new line. Ask for a new react component of an error popup.
 // 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+// 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
 // 4. To try out cursor on your own projects, go to file menu (top left) and open a folder.
 // 4. To try out cursor on your own projects, go to file menu (top left) and open a folder.
